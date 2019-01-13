3a6f60cc34fe40b1f8a2541e567be6cd9ab16aa5
tabata0330 <tabata@db.ics.keio.ac.jp>
2019-01-13 23:13:06 +0900

    fix escalation.js

--- skill/escalation.js
+++ skill/escalation.js
@@ -2,8 +2,8 @@
 const LINE_ADMIN_USER_ID = process.env.LINE_ADMIN_USER_ID;
 const debug = require("debug")("bot-express:skill");
 const request = require("request");
-Promise = require("bluebird");
-Promise.promisifyAll(request);
+// Promise = require("bluebird");
+// Promise.promisifyAll(request);
 
 module.exports = class Escalation {
     async finish(bot, event, context, resolve, reject){
