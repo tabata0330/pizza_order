191c5040c7e491f9393fba33b38ae02532cfc840
tabata0330 <tabata@db.ics.keio.ac.jp>
2019-01-13 21:50:51 +0900

    fix escalation.js

--- skill/escalation.js
+++ skill/escalation.js
@@ -13,18 +13,21 @@ module.exports = class Escalation {
             type: "text",
             text: "すぐ調べます。ちょっとお待ちを。"
         });
+        debug("送った");
         let sender_id = bot.extract_sender_id();
         let url = 'https://api.line.me/v2/bot/profile/' + sender_id;
+        debug("ヘッダー用意");
         let headers = {
             "Content-Type": "application/json",
             "Authorization": "Bearer " + process.env.LINE_ACCESS_TOKEN
         }
+        debug("レスポンスもらう");
         const response = await request.getAsync({
             url: url,
             headers: headers,
             json: true
         })
-        debug(`${response.body.userId}, ${response.body.displayName}`);
+        debug(`!!!!!!${response.body.userId}, ${response.body.displayName}`);
         let orig_message = JSON.parse(JSON.stringify(event.message));
         delete orig_message.id;
         debug(`!!!!!!!!!!orig_message: ${orig_message}`);
