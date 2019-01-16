14d8884d9b4ad0c0b9555afc8aee6c43e49c511d
tabata0330 <tabata@db.ics.keio.ac.jp>
2019-01-16 16:13:28 +0900

    fix insertdb

--- skill/insertdb.js
+++ skill/insertdb.js
@@ -3,11 +3,31 @@ const debug = require("debug")("bot-express:skill");
 
 
 module.exports = class Insertdb {
+    constructor() {
+        this.required_parameter = {
+            pizza: {
+                message_to_confirm: {
+                    type: "text",
+                    altText: "登録したいピザを教えてください",
+                }
+            },
+            size: {
+                message_to_confirm: {
+                    type: "text",
+                    altText: "登録したいサイズを教えてください",
+                }
+            }
+        };
+    }
+
     async finish(bot, event, context, resolve, reject){
+        let pizza = context.confirmed.pizza;
+        let size = context.confirmed.size;
+
         let messages = [
         	{
         		type: "text",
-        		text: "データベースに入れるよ〜"
+        		text: `${pizza}, ${size}をデータベースに入れるよ〜`
         	}
         ];
         await bot.reply(messages);
